2aa41a1f65e73dbfd2e4b7be073f6d4e1a6e0d04
ShalamarII <hitmanlee3@gmail.com>
2026-08-08 21:37:30 -0400

    adding hyprland-config

---
+++ hyprland-config/myThemePicker.lua
@@ -0,0 +1,5 @@
+# Requirements:
+# - Needs to take in all files from ~/.config/themes
+# - Needs to use those files in a PROGRAMMATIC WAY ("Primary, Secondary, Tertiary, Toolbar, Reveal, etc.")
+# - Needs to have a GUI to make easy to use (QuickShell)
+
